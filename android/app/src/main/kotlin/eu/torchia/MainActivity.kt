1f33167a5f4dedbbd668bfdae22f09365b513633
Cesare Torchia <cesare@parkingmycar.it>
2024-04-26 16:28:08 +0200

    template init

---
+++ android/app/src/main/kotlin/eu/torchia/MainActivity.kt
@@ -0,0 +1,6 @@
+package eu.torchia.trackus
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:trackus/app/app.dart';
+import 'package:trackus/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
